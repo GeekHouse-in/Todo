e226155d519d6925364ae63f0260bab30debd58d
naman <namanaggarwal45@gmail.com>
2022-02-19 21:46:44 +0530

    edit and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-
-import React from 'react'
-import './App.css'
-
+import React,{useState} from 'react'
 
 const App = () => {
 
+ 
+
+  const[count,setCount] = useState(0);
+  const handleClick = () =>{
+    setCount(count + 1);
+
+  }
+
   return (
-    <div>Hello</div>
-  )
-
-
+    <>
+      <div>{count}</div>
+      <button onClick={handleClick}>Click Me</button>  
+    </>
+    )
 }
 
 export default App
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {v4 as uuid} from 'uuid'
 import icon from './images/icon.png'
 import './Todo.css';
 
@@ -6,9 +7,14 @@ import './Todo.css';
 
 const Todo = () => {
 
+    const unique_id = uuid();
+    const small_id = unique_id.slice(0,8);
+    const [toggle,setToggle] = useState(false);
     const[data,setData] = useState([]);
+    const [editIemId, setEditItemId] = useState(null);
     const[inputData,setInputData] = useState('');
 
+    
     const handleChange = (e) =>{
 
         setInputData(e.target.value);
@@ -19,14 +25,50 @@ const Todo = () => {
         if(inputData === ''){
             alert('Enter Something');
         }
-        else {
-        setData([...data,inputData]);
-        setInputData('');
+        else if(inputData && toggle){
+
+            setData(
+
+                data.map((ele)=>{
+                    if(ele.id === editIemId){
+                        return {...ele,description:inputData}
+                    }
+                    return ele;
+                })
+            )
+            setToggle(false);
         }
+        else {
+        setData([...data,{
+            id : small_id,
+            description : inputData
+        }]);
+    
+        }
+        setInputData('');
+    }
+
+    const handleEdit = (id) =>{
+        setToggle(true);
+        setEditItemId(id);
+        const previousValue = data.find(ele=>ele.id == id);
+
+        setInputData(previousValue.description);
+
     }
  
-    
 
+    const deleteItem = (id) =>{
+        
+        const dataAfterDeletion = data.filter((ele)=>{
+            return id !== ele.id
+        })
+        setData(dataAfterDeletion)
+    }
+    const deleteAll = () => {
+        setData([]);
+    }
+ 
     return (
     <div className="Todo">
         <div style={{textAlign : "center",paddingTop : "5%"}}>
@@ -39,23 +81,35 @@ const Todo = () => {
                     <br />
                     <br />
                     <input type="text" placeholder='Enter your TODO' onChange={handleChange} value={inputData} />
-                    <button className="addBtn" onClick={addItem}><i className="fa fa-plus"></i></button>
+                    {
+
+                        (toggle === false) ? (
+                            <button className="addBtn" onClick={addItem}><i className="fa fa-plus"></i></button>
+                        ) : (<button className ="inputEdit" onClick={addItem} ><i className="fa fa-edit"></i></button>)
+
+                    }
+                    
+                    
+                    
+
                 </div>
                 <br />
+                
 
                 <div className="listItems">
                     {
                         data.map((ele)=>{
                             return (
-                                <div className="eachItem">
-                                    <span>{ele}</span>
-                                    <button className='deleteBtn'><i className='fa fa-trash'></i></button>
+                                <div key={ele.id} className="eachItem">
+                                    <span>{ele.description}</span>
+                                    <button className='deleteBtn'  onClick={()=> deleteItem(ele.id)}  ><i className='fa fa-trash'></i></button>
+                                    <button className='editBtn' onClick={()=>handleEdit(ele.id)} ><i className='fa fa-edit'></i></button>
                                 </div>
                             )
                         })
 }
-                    
-                </div>
+                    <button onClick={deleteAll}>Delete All</button>
+                </div> 
             </div>
         </div>
         
